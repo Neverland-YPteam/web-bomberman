6ea069ad49dd27c4ab6507f228e51976c251c7b4
vitalymuravyev <vitalymuravyev@gmail.com>
2022-09-02 17:03:17 +0300

    feat(router): add router and change structure

---
+++ packages/client/src/components/organisms/app/App.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { AuthPage } from '../../pages/auth';
+
+function App() {
+  useEffect(() => {
+    const fetchServerData = async () => {
+      const response = await fetch('http://localhost:3001')
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchServerData()
+  }, [])
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Routes>
+          // пока тут будет авторизация
+          <Route path="/" element={<AuthPage />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ packages/client/src/components/pages/auth/Auth.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const Auth = () => {
+  return (
+    <Box
+    sx={{
+      height: '100vh',
+      display: 'flex',
+    }}
+    >
+        <p>Authorization page</p>
+    </Box>
+
+  );
+};
+
+export default Auth;
