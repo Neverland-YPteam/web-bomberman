a2872ce6995b1a0e428b168c73fb6c6d385c5560
vitalymuravyev <44782070+vitalymuravyev@users.noreply.github.com>
2022-09-07 11:18:08 +0300

    feat(auth): change auth page

--- packages/client/src/components/pages/auth/Auth.tsx
+++ packages/client/src/components/pages/auth/Auth.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Box, Button, Paper, TextField, Typography } from '@mui/material'
+import { TextField } from '@mui/material'
+import { FormContainer } from '@molecules/form-container'
+import { SubmitButton } from '@atoms/submit-button'
+import { FormLink } from '@atoms/form-link'
 
 const Auth = () => {
   const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
@@ -12,62 +15,31 @@ const Auth = () => {
   }
 
   return (
-    <Box
-    sx={{
-      height: '100vh',
-      display: 'flex',
-    }}
+    <FormContainer
+      onFormSubmit={handleSubmit}
+      title="Авторизация"
     >
-      <Paper
-        variant="outlined"
-        sx={{
-          maxWidth: '330px',
-          margin: 'auto',
-          textAlign: 'center',
-          padding: '24px 28px',
-        }}
-      >
-        <Typography component="h2" variant="h5">
-          Авторизация
-        </Typography>
-        <Box
-          component="form"
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-          onSubmit={handleSubmit}
-        >
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Логин"
-            name="login"
-            autoComplete="login"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Пароль"
-            type="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2, bgcolor: ({palette}) => palette.info.main }}
-          >
-            ВОЙТИ
-          </Button>
-        </Box>
-      </Paper>
-    </Box>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        label="Логин"
+        name="login"
+        autoComplete="login"
+        autoFocus
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Пароль"
+        type="password"
+        autoComplete="current-password"
+      />
+      <SubmitButton>ВОЙТИ</SubmitButton>
+      <FormLink to="/sign-in" text="Нет аккаунта? Регистрация" />
+    </FormContainer>
   );
 };
 
