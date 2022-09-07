da563c15055375654b7b0306c25d65c9b573222c
vitalymuravyev <44782070+vitalymuravyev@users.noreply.github.com>
2022-09-07 10:11:41 +0300

    feat(auth): add form container

---
+++ packages/client/src/components/molecules/form-container/FormContainer.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Box, Paper, Typography } from '@mui/material'
+
+interface Props {
+  onFormSubmit: (evt: React.FormEvent<HTMLFormElement>) => void;
+  children: React.ReactNode;
+  title?: string
+}
+
+const FormContainer = ({ onFormSubmit, children, title }: Props) => {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'flex',
+      }}
+    >
+      <Paper
+        variant="outlined"
+        sx={{
+          maxWidth: '330px',
+          margin: 'auto',
+          textAlign: 'center',
+          padding: '24px 28px',
+        }}
+      >
+        {title && (
+          <Typography component="h2" variant="h5">
+            {title}
+          </Typography>
+        )}
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+          onSubmit={onFormSubmit}
+        >
+          {children}
+        </Box>
+      </Paper>
+    </Box>
+  )
+}
+
+export default FormContainer
