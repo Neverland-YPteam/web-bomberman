d05b1b501ae2df99dfd561e416c8a5bf36156bec
vitalymuravyev <vitalymuravyev@gmail.com>
2022-09-09 18:41:16 +0300

    feat(app): add error boundary

--- packages/client/src/components/organisms/app-routes/AppRoutes.tsx
+++ packages/client/src/components/organisms/app-routes/AppRoutes.tsx
@@ -3,15 +3,18 @@ import { Route, Routes } from 'react-router-dom'
 import { AuthPage } from '@pages/auth'
 import { SignupPage } from '@pages/sign-up'
 import Leaderboard from '@pages/Leaderboard'
+import { ErrorBoundary } from '@organisms/error-boundary'
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      // пока тут будет авторизация
-      <Route path="/" element={<AuthPage />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-      <Route path="/sign-up" element={<SignupPage />} />
-    </Routes>
+    <ErrorBoundary>
+      <Routes>
+        // пока тут будет авторизация
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+      </Routes>
+    </ErrorBoundary>
   )
 }
 
