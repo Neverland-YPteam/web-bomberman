6df8746e82b67c11cc056819bb36b447d6266fbe
kokoccc <kokoc@kokoc.cc>
2022-09-10 06:00:50 +0300

    test js import

---
+++ packages/client/src/components/pages/game/Game.tsx
@@ -0,0 +1,56 @@
+import {
+  Box,
+} from '@mui/material'
+import { useEffect } from 'react'
+
+import { loadResources } from './js'
+
+const Game = () => {
+  useEffect(() => {
+    loadResources()
+  }, [])
+
+  return (
+    <Box sx={{ height: '100vh' }} >
+      <Box
+        sx={{
+          position: 'relative',
+          width: 'min(100vw, calc(100vh * 4 / 3))',
+          maxWidth: 800,
+          aspectRatio: '4 / 3',
+          margin: '0 auto',
+        }}
+      >
+        <canvas
+          id="game_static"
+          className="game__canvas"
+          width="1200"
+          height="900"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        ></canvas>
+
+        <canvas
+          id="game_dynamic"
+          className="game__canvas"
+          width="1200"
+          height="900"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        ></canvas>
+      </Box>
+    </Box>
+  )
+}
+
+export default Game
---
+++ packages/client/src/components/pages/game/js/index.js
@@ -0,0 +1,8 @@
+const loadResources = async () => {
+  console.log('Загружаем ресурсы')
+}
+
+const canvas = document.querySelector('.game__canvas')
+console.log(canvas);
+
+export { loadResources }
