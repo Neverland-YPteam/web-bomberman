0544038cf36d967f67cd5bd063e85beb5e3caf79
vitalymuravyev <44782070+vitalymuravyev@users.noreply.github.com>
2022-09-16 15:13:00 +0300

    build: add redux and redux-thunk

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,11 +4,15 @@ import { App } from '@organisms/app'
 import './normalize.css'
 import './index.css'
 import { AppThemeProvider } from '@services/AppThemeProvider'
+import { Provider } from 'react-redux'
+import { store } from '@services/store/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppThemeProvider>
-      <App />
-    </AppThemeProvider>
+    <Provider store={store}>
+      <AppThemeProvider>
+        <App />
+      </AppThemeProvider>
+    </Provider>
   </React.StrictMode>
 )
