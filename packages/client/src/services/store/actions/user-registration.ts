77edd7ef9db7a046051d93c5f97bb561e4490ad2
vitalymuravyev <vitalymuravyev@gmail.com>
2022-09-19 19:29:55 +0300

    feat(user): add registration actions and thunk

---
+++ packages/client/src/services/store/actions/user-registration.ts
@@ -0,0 +1,53 @@
+import { redirect } from "react-router-dom";
+import { AppDispatch } from '@src/types/store'
+import { API_URL } from '@utils/constants'
+import { checkResponseStatus } from '@utils/helpers'
+
+export const USER_REGISTRATION_REQUEST = 'USER_REGISTRATION_REQUEST';
+export const USER_REGISTRATION_SUCCESS = 'USER_REGISTRATION_SUCCESS';
+export const USER_REGISTRATION_FAILED = 'USER_REGISTRATION_FAILED';
+
+interface IGetRegistrationAction {
+  readonly type: typeof USER_REGISTRATION_REQUEST;
+}
+interface IGetRegistrationSuccessAction {
+  readonly type: typeof USER_REGISTRATION_SUCCESS;
+}
+interface IGetRegistrationFailedAction {
+  readonly type: typeof USER_REGISTRATION_FAILED;
+}
+
+export type TUserRegistrationActions =
+  IGetRegistrationAction
+  | IGetRegistrationSuccessAction
+  | IGetRegistrationFailedAction
+
+export const registerUser = (data: FormData) => {
+  return function(dispatch: AppDispatch) {
+    dispatch({
+      type: USER_REGISTRATION_REQUEST
+    })
+    fetch(`${API_URL}/auth/signup`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(Object.fromEntries(data)),
+    })
+      .then(res => checkResponseStatus(res))
+      .then(res => res.json())
+      .then(result => {
+        console.log(result)
+        dispatch({
+          type: USER_REGISTRATION_SUCCESS,
+        })
+        // redirect('/profile')
+      })
+      .catch(() => {
+        dispatch({
+          type: USER_REGISTRATION_FAILED,
+        })
+      })
+  }
+}
