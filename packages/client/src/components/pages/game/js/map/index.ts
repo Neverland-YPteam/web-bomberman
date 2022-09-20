250d257d3e98cb484987749dcf980b48a8e7f3ef
kokoccc <kokoc@kokoc.cc>
2022-09-20 10:12:05 +0300

    type bomb, map, panel

--- packages/client/src/components/pages/game/js/map/index.ts
+++ packages/client/src/components/pages/game/js/map/index.ts
@@ -3,15 +3,15 @@
  * Используется level'ом
  */
 
-import {
+ import {
   PANEL_HEIGHT_PX,
   MAP_TILES_COUNT_X,
   MAP_TILES_COUNT_Y,
   TILE_SIZE,
   textures,
-} from './const'
+} from '../const'
 
-import { canvasStatic } from './canvas'
+import { canvasStatic } from '../canvas'
 
 const {
   TEXTURE_COLUMN,
@@ -19,26 +19,7 @@ const {
 } = textures
 
 class Map {
-  _drawHorizondalBorders() {
-    let i = 0
-
-    while (i < MAP_TILES_COUNT_X) {
-      canvasStatic.image(TEXTURE_COLUMN, i * TILE_SIZE, PANEL_HEIGHT_PX)
-      canvasStatic.image(TEXTURE_COLUMN, i * TILE_SIZE, canvasStatic.height - TILE_SIZE)
-      i++
-    }
-  }
-
-  _drawOtherBorders() {
-    let rowIndex = 1
-
-    while (rowIndex < MAP_TILES_COUNT_Y) {
-      canvasStatic.image(TEXTURE_COLUMN, 0, PANEL_HEIGHT_PX + rowIndex)
-      rowIndex++
-    }
-  }
-
-  drawTexture(texture, col, row) {
+  drawTexture(texture: number, col: number, row: number) {
     const x = col * TILE_SIZE
     const y = PANEL_HEIGHT_PX + row * TILE_SIZE
 
