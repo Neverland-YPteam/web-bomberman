c61cdb448b8e70895e9d95eb93b2cb73d797c751
kokoccc <kokoc@kokoc.cc>
2022-09-29 22:54:54 +0300

    generate damaged column textures

--- packages/client/src/components/pages/game/scripts/map/index.ts
+++ packages/client/src/components/pages/game/scripts/map/index.ts
@@ -3,6 +3,8 @@
  * Используется level'ом
  */
 
+import { TTextures } from './types'
+
 import {
   PANEL_HEIGHT_PX,
   MAP_TILES_COUNT_X,
@@ -11,17 +13,34 @@ import {
   textures,
 } from '../const'
 
+import { getRandomNumberBetween } from '../utils'
 import { canvasStatic } from '../canvas'
 
 const {
-  TEXTURE_COLUMN,
-  TEXTURE_GRASS,
+  TEXTURE_COLUMN, TEXTURE_COLUMN_DAMAGED_1, TEXTURE_COLUMN_DAMAGED_2, TEXTURE_COLUMN_DAMAGED_3, TEXTURE_GRASS,
 } = textures
 
+const DAMAGED_COLUMN_RATIO = 20
+
+const textureColumnsDamaged = [TEXTURE_COLUMN_DAMAGED_1, TEXTURE_COLUMN_DAMAGED_2, TEXTURE_COLUMN_DAMAGED_3]
+
 class Map {
+  private _textures: TTextures = {}
+
+  private _getRandomColumnTexture() {
+    const damagedColumnTextureIndex = getRandomNumberBetween(0, DAMAGED_COLUMN_RATIO)
+    return textureColumnsDamaged[damagedColumnTextureIndex] ?? TEXTURE_COLUMN
+  }
+
+  getTexture(col: number, row: number) {
+    return this._textures[`${col}-${row}`]
+  }
+
   drawTexture(texture: number, col: number, row: number) {
     const x = col * TILE_SIZE
     const y = PANEL_HEIGHT_PX + row * TILE_SIZE
+
+    this._textures[`${col}-${row}`] = texture
 
     canvasStatic.image(texture, x, y)
   }
@@ -30,8 +49,8 @@ class Map {
     let rowIndex = 0
 
     while (rowIndex < MAP_TILES_COUNT_Y) {
-      this.drawTexture(TEXTURE_COLUMN, 0, rowIndex)
-      this.drawTexture(TEXTURE_COLUMN, MAP_TILES_COUNT_X - 1, rowIndex)
+      this.drawTexture(this._getRandomColumnTexture(), 0, rowIndex)
+      this.drawTexture(this._getRandomColumnTexture(), MAP_TILES_COUNT_X - 1, rowIndex)
 
       const isEdge = rowIndex === 0 || rowIndex === MAP_TILES_COUNT_Y - 1
 
@@ -39,7 +58,7 @@ class Map {
         let colIndex = 1
 
         while (colIndex < MAP_TILES_COUNT_X - 1) {
-          this.drawTexture(TEXTURE_COLUMN, colIndex, rowIndex)
+          this.drawTexture(this._getRandomColumnTexture(), colIndex, rowIndex)
           colIndex++
         }
       }
@@ -48,7 +67,10 @@ class Map {
         let colIndex = 1
 
         while (colIndex < MAP_TILES_COUNT_X - 1) {
-          const texture = rowIndex % 2 || colIndex % 2 ? TEXTURE_GRASS : TEXTURE_COLUMN
+          const texture = rowIndex % 2 === 0 && colIndex % 2 === 0
+            ? TEXTURE_COLUMN
+            : TEXTURE_GRASS
+
           this.drawTexture(texture, colIndex, rowIndex)
           colIndex++
         }
