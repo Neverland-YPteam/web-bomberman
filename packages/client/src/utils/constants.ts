47674197361de6b285f6eab0cd2261c555c9edbb
kokoccc <kokoc@kokoc.cc>
2022-10-13 20:30:36 +0300

    review fixes

--- packages/client/src/utils/constants.ts
+++ packages/client/src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const API_URL = 'https://ya-praktikum.tech/api/v2'
-export const API_RESOURCE_URL = 'https://ya-praktikum.tech/api/v2/resources'
+export const API_RESOURCE_URL = `${API_URL}/resources`
