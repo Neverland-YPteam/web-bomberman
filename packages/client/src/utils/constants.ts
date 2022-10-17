06668cfa879df08c37c0af999835e645fea69d1c
kokoccc <kokoc@kokoc.cc>
2022-10-17 23:29:08 +0300

    add url constants

--- packages/client/src/utils/constants.ts
+++ packages/client/src/utils/constants.ts
@@ -1,2 +1,4 @@
 export const API_URL = 'https://ya-praktikum.tech/api/v2'
 export const API_RESOURCE_URL = `${API_URL}/resources`
+export const OAUTH_YANDEX_AUTHORIZE_URL = `https://oauth.yandex.ru/authorize?response_type=code`
+export const OAUTH_REDIRECT_URI = `http://localhost:3000`
