ca64fbc4f724ee147fd7d3595cf156bba2d6c93f
kokoccc <kokoc@kokoc.cc>
2022-10-22 17:37:52 +0300

    useLayoutEffect => useEffect

--- packages/client/src/components/organisms/app/App.tsx
+++ packages/client/src/components/organisms/app/App.tsx
@@ -5,13 +5,26 @@ import { AppRoutes } from '@organisms/app-routes'
 import { Box } from '@mui/material';
 import { useDispatch } from '@utils/hooks'
 import { loadUser } from '@services/store/actions/user'
+import { USER_LOGIN_SUCCESS } from '@services/store/actions/user-auth'
+import { tryOAuth } from '@services/OAuth'
 
 function App() {
   const dispatch: any = useDispatch()
 
   useEffect(() => {
-    dispatch(loadUser())
-  }, [])
+    const oAuthCode = new URLSearchParams(window.location.search).get('code')
+    const isSessionActive = localStorage.getItem('Session') === 'active'
+
+    if (oAuthCode) {
+      tryOAuth(oAuthCode, dispatch)
+    } else if (isSessionActive) {
+      dispatch({
+        type: USER_LOGIN_SUCCESS
+      })
+
+      dispatch(loadUser())
+    }
+  }, [dispatch])
 
   return (
     <BrowserRouter>
