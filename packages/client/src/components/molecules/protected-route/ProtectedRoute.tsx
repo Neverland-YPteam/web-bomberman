9ad966321bc9c0ea158464655b63781a45c46b4c
kokoccc <kokoc@kokoc.cc>
2022-10-24 20:35:29 +0300

    fix protected route component

--- packages/client/src/components/molecules/protected-route/ProtectedRoute.tsx
+++ packages/client/src/components/molecules/protected-route/ProtectedRoute.tsx
@@ -3,10 +3,11 @@ import { useSelector } from '@utils/hooks'
 import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
 const ProtectedRoute = () => {
+  const isSessionActive = localStorage.getItem('Session') === 'active'
   const { isUserAuth, loginRequest } = useSelector(state => state.userAuth)
   const location = useLocation()
 
-  if (!isUserAuth && !loginRequest) {
+  if (!isUserAuth && !loginRequest && !isSessionActive) {
     return <Navigate to="/" state={{ from: location }} />
   }
 
