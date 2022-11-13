d014f714901214afa9919b0e1b9e5d658ede4056
vitalymuravyev <vitalymuravyev@gmail.com>
2022-11-13 07:48:23 +0300

    ssr try fix styles

--- packages/client/src/entry-server.tsx
+++ packages/client/src/entry-server.tsx
@@ -4,12 +4,15 @@ import { StaticRouter } from 'react-router-dom/server'
 import { App } from 'client/src/components/organisms/app'
 import { Provider } from 'react-redux'
 import { store } from '@services/store/store'
+import { AppThemeProvider } from '@services/AppThemeProvider'
 
 export async function SSRender (url: string | Partial<Location>) {
   return renderToString(
     <Provider store={store}>
       <StaticRouter location={url}>
-        <App />
+          <AppThemeProvider>
+            <App />
+          </AppThemeProvider>
       </StaticRouter>
     </Provider>
   )
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -6,15 +6,21 @@ import './styles/index.css'
 import { AppThemeProvider } from '@services/AppThemeProvider'
 import { Provider } from 'react-redux'
 import { store } from '@services/store/store'
+import { CacheProvider } from '@emotion/react'
+import createEmotionCache from '@utils/createEmotionCache'
+
+const cache = createEmotionCache()
 
 ReactDOM.hydrateRoot(
   document.getElementById('App') as HTMLElement,
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <AppThemeProvider>
-          <App />
-        </AppThemeProvider>
+        <CacheProvider value={cache}>
+          <AppThemeProvider>
+            <App />
+          </AppThemeProvider>
+        </CacheProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
