356db15e826642fe6422216353a35571257426a6
kokoccc <kokoc@kokoc.cc>
2022-11-15 10:06:19 +0300

    init db correctly

--- packages/server/db/index.ts
+++ packages/server/db/index.ts
@@ -1,25 +1,21 @@
-import { Sequelize, SequelizeOptions } from 'sequelize-typescript'
+import { Sequelize } from 'sequelize-typescript'
 import { SiteTheme, User, UserTheme} from '../db/models'
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } = process.env
-
-const sequelizeOptions: SequelizeOptions = {
-  host: 'localhost',
-  port: POSTGRES_PORT as unknown as number,
-  username: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-  database: POSTGRES_DB,
-  dialect: 'postgres',
-  models: [SiteTheme, User, UserTheme]
-}
-
-export const sequelize = new Sequelize(sequelizeOptions)
+export const sequelize = new Sequelize(
+  'postgres',
+  'postgres',
+  'postgres',
+  {
+    host: 'postgres',
+    port: 5432,
+    dialect: 'postgres',
+    models: [SiteTheme, User, UserTheme],
+  }
+)
 
 export const dbConnect = async () => {
   try {
-    console.log('########## TRYING TO AUTHENTICATE…')
     await sequelize.authenticate()
-    console.log('########## TRYING TO SYNC…')
     await sequelize.sync()
     console.log('Connection has been established successfully.')
   } catch (error) {
