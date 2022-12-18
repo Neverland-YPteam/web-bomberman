5515d89dcb0d18b03498b8e9dd00540f59a68471
kokoccc <kokoc@kokoc.cc>
2022-12-18 04:13:32 +0300

    oauth fix

--- packages/client/src/utils/constants.ts
+++ packages/client/src/utils/constants.ts
@@ -12,7 +12,7 @@ export const API_LOCAL_BASE_URL = `http://localhost:${__SERVER_PORT__}`
 export const API_LOCAL_USER_URL = `${API_LOCAL_BASE_URL}/user`
 
 export const OAUTH_YANDEX_AUTHORIZE_URL = `https://oauth.yandex.ru/authorize?response_type=code`
-export const OAUTH_REDIRECT_URI = `http://localhost:3000`
+export const OAUTH_REDIRECT_URI = IS_PROD ? location.origin : `http://localhost:3000`
 
 export const TEAM_NAME = 'bomberman'
 export const RATING_FIELD_NAME = 'score'
